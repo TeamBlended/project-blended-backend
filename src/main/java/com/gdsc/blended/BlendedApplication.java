34454ce282f151b4712e9ce6c3b56b720b966c1e
Reese <ajw5921@gmail.com>
2023-04-11 01:13:43 +0900

    Project Init

---
+++ src/main/java/com/gdsc/blended/BlendedApplication.java
@@ -0,0 +1,13 @@
+package com.gdsc.blended;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlendedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlendedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gdsc/blended/BlendedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdsc.blended;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlendedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
