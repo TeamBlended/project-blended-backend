23a1501670d93be29bfdf0ea8f010ea9cb715c6d
jsangmin99 <jsangmin99@gmail.com>
2023-04-23 01:14:30 +0900

    게시글 작성 초안 crud 작성

--- src/main/java/com/gdsc/blended/post/repository/PostRepository.java
+++ src/main/java/com/gdsc/blended/post/repository/PostRepository.java
@@ -1,4 +1,12 @@
 package com.gdsc.blended.post.repository;
 
-public class PostRepository {
+import com.gdsc.blended.post.entity.PostEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<PostEntity, Long> {
+    @Query("SELECT p FROM PostEntity p ORDER BY p.id DESC")
+    List<PostEntity> findAllDesc();
 }
--- src/main/java/com/gdsc/blended/post/service/PostService.java
+++ src/main/java/com/gdsc/blended/post/service/PostService.java
@@ -1,4 +1,61 @@
 package com.gdsc.blended.post.service;
 
+import com.gdsc.blended.category.entity.CategoryEntity;
+import com.gdsc.blended.category.repository.CategoryRepository;
+import com.gdsc.blended.post.dto.PostRequestDto;
+import com.gdsc.blended.post.dto.PostResponseDto;
+import com.gdsc.blended.post.entity.PostEntity;
+import com.gdsc.blended.post.repository.PostRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@Service
 public class PostService {
+    private final PostRepository postRepository;
+    private final CategoryRepository categoryRepository;
+
+    public PostEntity findById(Long id) {
+        return postRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid post id"));
+    }
+
+    public List<PostResponseDto> getAllPost() {
+        return postRepository.findAllDesc().stream()
+                .map(PostResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public PostResponseDto createPost(PostRequestDto postRequestDto, Long categoryId ) {
+        CategoryEntity category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid category id"));
+        PostEntity savedPost = postRepository.save(postRequestDto.toEntity(category));
+        return new PostResponseDto(savedPost);
+    }
+
+    @Transactional
+    public void deletePost(Long postId) {
+        PostEntity postEntity = findById(postId);
+
+        postRepository.delete(postEntity);
+    }
+
+
+    //TODO .. 만약에 기존 모집인원이 4명이여서 3명 참가했는데 2명으로 수정한다면?,,,
+
+    public PostResponseDto updatePost(Long postId, PostRequestDto postRequestDto) {
+        PostEntity postEntity = findById(postId);
+
+        postEntity.setTitle(postRequestDto.getTitle());
+        postEntity.setContent(postRequestDto.getContent());
+        postEntity.setMaxRecruits(postRequestDto.getMaxRecruit());
+
+        PostEntity updatedPost = postRepository.save(postEntity);
+
+        return new PostResponseDto(updatedPost);
+    }
 }
