7259b7efa2d83a5d1be63e84107f2502bc580591
Reese <ajw5921@gmail.com>
2023-04-23 23:58:22 +0900

    feat: 구글 OAuth2 인증을 위한 코드 추가

---
+++ src/main/java/com/gdsc/blended/user/dto/SessionUser.java
@@ -0,0 +1,17 @@
+package com.gdsc.blended.user.dto;
+
+import com.gdsc.blended.user.entity.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+    }
+}
