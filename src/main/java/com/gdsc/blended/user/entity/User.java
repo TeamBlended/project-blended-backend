b048ee487a63679bc1a4ba49f414088d448727f6
Reese <ajw5921@gmail.com>
2023-05-05 14:06:41 +0900

    fix: syntax error 해결

--- src/main/java/com/gdsc/blended/user/entity/User.java
+++ src/main/java/com/gdsc/blended/user/entity/User.java
@@ -28,17 +28,15 @@ public class User {
 
     private String provider;
 
-    private String location;
-
-
+    //private String location;
+    
 
     @Builder
-    public User(Long userId, String name, String email, String password, UserType type){
+    public User(Long userId, String name, String email, UserType type){
         this.userId = userId;
         this.name = name;
         this.email = email;
         this.type = type;
-        this.location = location;
     }
 
     public User(Long userId){
