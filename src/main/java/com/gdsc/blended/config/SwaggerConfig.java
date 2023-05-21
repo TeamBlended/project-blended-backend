3553747d3563b6b3b30bb5b948e4a51a9e4a6ce8
Reese <ajw5921@gmail.com>
2023-05-21 18:53:22 +0900

    fix: swagger error

--- src/main/java/com/gdsc/blended/config/SwaggerConfig.java
+++ src/main/java/com/gdsc/blended/config/SwaggerConfig.java
@@ -1,25 +1,41 @@
 package com.gdsc.blended.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.springdoc.core.GroupedOpenApi;
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
+@RequiredArgsConstructor
 @Configuration
 public class SwaggerConfig {
-    @Bean
-    public GroupedOpenApi publicApi() {
-        return GroupedOpenApi.builder()
-                .group("v1-definition")
-                .pathsToMatch("/api/**")
-                .build();
-    }
-    @Bean
-    public OpenAPI springShopOpenAPI() {
+    public OpenAPI BlendedApi() {
+        Info info = new Info()
+                .title("Blended 프로젝트 API") // 타이틀
+                .version("v1") // 문서 버전
+                .description("블랜디드 백엔드 API 명세서");
+
+        // security 스키마 설정
+        SecurityScheme bearerAuth = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("Blended")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name(HttpHeaders.AUTHORIZATION);
+
+        // Security 요청 설정
+        SecurityRequirement addSecurityItem = new SecurityRequirement();
+        addSecurityItem.addList("JWT");
+
         return new OpenAPI()
-                .info(new Info().title("Blended API")
-                        .description("블랜디드 백엔드 API 명세서")
-                        .version("v0.0.2"));
+                // Security 인증 컴포넌트 설정
+                .components(new Components().addSecuritySchemes("JWT", bearerAuth))
+                // API 마다 Security 인증 컴포넌트 설정
+                .addSecurityItem(addSecurityItem)
+                .info(info);
     }
+
 }
