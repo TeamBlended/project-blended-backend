0f7277115f694ee32644b2e6cfa7551ca3a06f75
Reese <ajw5921@gmail.com>
2023-05-21 18:53:22 +0900

    fix: swagger error

--- src/main/java/com/gdsc/blended/config/SecurityConfig.java
+++ src/main/java/com/gdsc/blended/config/SecurityConfig.java
@@ -15,7 +15,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
