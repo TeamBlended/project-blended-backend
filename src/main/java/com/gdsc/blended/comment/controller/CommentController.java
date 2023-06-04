465b7c54c9bacd08161b18cfbed382c9c2e0ff63
Reese <ajw5921@gmail.com>
2023-06-04 21:50:00 +0900

    fix: user id -> email

--- src/main/java/com/gdsc/blended/comment/controller/CommentController.java
+++ src/main/java/com/gdsc/blended/comment/controller/CommentController.java
@@ -3,6 +3,7 @@ package com.gdsc.blended.comment.controller;
 import com.gdsc.blended.comment.dto.CommentRequestDto;
 import com.gdsc.blended.comment.dto.CommentResponseDto;
 import com.gdsc.blended.comment.service.CommentService;
+import com.gdsc.blended.jwt.oauth.UserInfo;
 import com.gdsc.blended.user.entity.UserEntity;
 import io.swagger.v3.oas.annotations.Operation;
 import org.apache.catalina.User;
@@ -24,16 +25,18 @@ public class CommentController {
 
     @Operation(summary = "댓글작성")
     @PostMapping()
-    public ResponseEntity<CommentResponseDto> createComment(@RequestBody CommentRequestDto requestDto, @PathVariable Long postId , @AuthenticationPrincipal UserEntity user) {
-        CommentResponseDto responseDto = commentService.createComment(requestDto,postId, user.getId());
+    public ResponseEntity<CommentResponseDto> createComment(@RequestBody CommentRequestDto requestDto, @PathVariable Long postId , @AuthenticationPrincipal UserInfo user) {
+        CommentResponseDto responseDto = commentService.createComment(requestDto,postId, user.getEmail());
         return ResponseEntity.ok(responseDto);
     }
+
     @Operation(summary = "댓글조회(필요없을듯)")
     @GetMapping("/{commentId}")
     public ResponseEntity<CommentResponseDto> getcomment(@PathVariable Long commentId){
         CommentResponseDto comment = commentService.getComments(commentId);
         return ResponseEntity.ok(comment);
     }
+
     @Operation(summary = "해당게시글에 속한 댓글 리스트")
     @GetMapping()
     public ResponseEntity<List<CommentResponseDto>> getCommentListByPost(@PathVariable Long postId) {
@@ -43,23 +46,23 @@ public class CommentController {
 
     @Operation(summary = "댓글 수정")
     @PutMapping("/{commentId}")
-    public ResponseEntity<CommentResponseDto> updateComment(@RequestBody CommentRequestDto requestDto, @PathVariable Long commentId, @AuthenticationPrincipal UserEntity user){
-        CommentResponseDto updateComment = commentService.updateComment(requestDto, commentId, user.getId());
+    public ResponseEntity<CommentResponseDto> updateComment(@RequestBody CommentRequestDto requestDto, @PathVariable Long commentId, @AuthenticationPrincipal UserInfo user){
+        CommentResponseDto updateComment = commentService.updateComment(requestDto, commentId, user.getEmail());
         return ResponseEntity.ok(updateComment);
     }
 
     //삭제하기(근데 내용만 안보이게)
     @Operation(summary = "댓글 삭제하기", description = "실제로 삭제되진 않고 내용만 지워짐")
     @PutMapping("/{commentId}/delete")
-    public ResponseEntity<CommentResponseDto> deleteComment(@PathVariable Long commentId, @AuthenticationPrincipal UserEntity user){
-        CommentResponseDto deleteComment = commentService.deleteComment(commentId,user.getId());
+    public ResponseEntity<CommentResponseDto> deleteComment(@PathVariable Long commentId, @AuthenticationPrincipal UserInfo user){
+        CommentResponseDto deleteComment = commentService.deleteComment(commentId,user.getEmail());
         return ResponseEntity.ok(deleteComment);
     }
 
     @Operation(summary = "댓글 DB까지 삭제")
     @DeleteMapping("/{commentId}")
-    public ResponseEntity<Void> realDeleteComment(@PathVariable Long commentId, @AuthenticationPrincipal UserEntity user){
-        commentService.realDeleteComment(commentId, user.getId());
+    public ResponseEntity<Void> realDeleteComment(@PathVariable Long commentId, @AuthenticationPrincipal UserInfo user){
+        commentService.realDeleteComment(commentId, user.getEmail());
         return ResponseEntity.noContent().build();
     }
 
