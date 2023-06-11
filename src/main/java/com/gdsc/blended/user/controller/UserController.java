957b2c05d4d7a9523fb748db9cc436f816f3b5a8
Reese <ajw5921@gmail.com>
2023-06-11 14:09:01 +0900

    fix: import 에러 삭제

--- src/main/java/com/gdsc/blended/user/controller/UserController.java
+++ src/main/java/com/gdsc/blended/user/controller/UserController.java
@@ -2,15 +2,11 @@ package com.gdsc.blended.user.controller;
 
 
 import com.gdsc.blended.jwt.oauth.UserInfo;
-import com.gdsc.blended.user.dto.NicknameRequest;
 import com.gdsc.blended.user.dto.UserRequestDto;
-import com.gdsc.blended.user.dto.response.NicknameResponse;
 import com.gdsc.blended.user.dto.response.UserResponseDto;
-import com.gdsc.blended.user.entity.UserEntity;
 import com.gdsc.blended.user.repository.UserRepository;
 import com.gdsc.blended.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -30,18 +26,3 @@ public class UserController {
         return ResponseEntity.ok(updateddUser);
     }
 }
-
-
-/*    @PutMapping("/user/{userId}")
-    public ResponseEntity<NicknameResponse> updateNickname(@RequestBody NicknameRequest nicknameRequest, @AuthenticationPrincipal UserInfo user){
-        userService.updateNickname(user.getEmail(), nicknameRequest.nickname());
-        NicknameResponse nicknameResponse = new NicknameResponse(user.getEmail(), nicknameRequest.nickname());
-        return ResponseEntity.ok(nicknameResponse);
-    }*/
-
-//        UserResponseDto updateNickname = userService.updateNickname(nicknameRequest, user.getEmail());
-//        return ResponseEntity.status(HttpStatus.OK).body(updateNickname);
-
-/*userService.updateNickname(user.getEmail(), nicknameRequest.nickname());
-        NicknameResponse nicknameResponse = new NicknameResponse(user.getEmail(), nicknameRequest.nickname());
-        return ResponseEntity.ok(nicknameResponse);*/
