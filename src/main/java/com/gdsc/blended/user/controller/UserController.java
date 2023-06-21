8a50c0128ddbfe4072b68f9d20ccf2461ebadf83
Reese <ajw5921@gmail.com>
2023-06-21 22:06:27 +0900

    fix: DB 반영 수정

--- src/main/java/com/gdsc/blended/user/controller/UserController.java
+++ src/main/java/com/gdsc/blended/user/controller/UserController.java
@@ -2,27 +2,23 @@ package com.gdsc.blended.user.controller;
 
 
 import com.gdsc.blended.jwt.oauth.UserInfo;
-import com.gdsc.blended.user.dto.UserRequestDto;
-import com.gdsc.blended.user.dto.response.UserResponseDto;
-import com.gdsc.blended.user.repository.UserRepository;
+import com.gdsc.blended.user.entity.UserEntity;
 import com.gdsc.blended.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/users")
 public class UserController {
     private final UserService userService;
-    private final UserRepository userRepository;
 
-    @PutMapping("/user/{userId}")
-    public ResponseEntity<UserResponseDto> updateNickname(@PathVariable("userId") Long userId, @RequestBody UserRequestDto userRequestDto, @AuthenticationPrincipal UserInfo user) {
-        String userEmail = user.getEmail();
-        UserResponseDto updateddUser = userService.updateNickname(userRequestDto, userEmail);
-        return ResponseEntity.ok(updateddUser);
+    @PutMapping("/{userId}/nickname")
+    public UserEntity updateUserNickname(@AuthenticationPrincipal UserInfo user, @RequestParam("newNickname") String newNickname){
+        return userService.updateUserNickname(user.getEmail(), newNickname);
     }
+
 }
+
