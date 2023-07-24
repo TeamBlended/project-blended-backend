07dd65db1b2eabc2099016e4abb0eec27ddb579e
jeonsangmin <jsangmin99@gmail.com>
2023-07-24 16:35:06 +0900

    ImageUpload 분리

--- src/main/java/com/gdsc/blended/common/image/exception/ExceptionCode.java
+++ src/main/java/com/gdsc/blended/common/image/exception/ExceptionCode.java
@@ -3,7 +3,8 @@ package com.gdsc.blended.common.image.exception;
 import lombok.Getter;
 
 public enum ExceptionCode {
-    IMAGE_NOT_FOUND(404, "Image not found");
+    IMAGE_NOT_FOUND(404, "Image not found"), IMAGE_UPLOAD_FAILED(500, "Image upload failed"), IMAGE_DELETE_FAILED(500, "Image delete failed");
+
 
     @Getter
     private final int status;
