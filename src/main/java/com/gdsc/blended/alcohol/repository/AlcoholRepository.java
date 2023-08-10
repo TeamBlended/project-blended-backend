f3a529f7a592beab3fa7f69d1be518bc97f6ac3f
jeonsangmin <jsangmin99@gmail.com>
2023-08-10 15:24:02 +0900

    스웨거 설명 추가

--- src/main/java/com/gdsc/blended/alcohol/repository/AlcoholRepository.java
+++ src/main/java/com/gdsc/blended/alcohol/repository/AlcoholRepository.java
@@ -10,5 +10,4 @@ import java.util.Optional;
 public interface AlcoholRepository extends JpaRepository<AlcoholEntity, Long> {
     // TODO: 2023/08/10 영어 같은경우는 대소문자 구별을 못하는 문제 있음 알고리즘 수정해야함 ㅠㅠ
     List<AlcoholEntity> findByWhiskyKoreanContainingOrWhiskyEnglishContaining(String koreanKeyword, String englishKeyword);
-    Optional<AlcoholEntity> findByWhiskyKoreanOrWhiskyEnglish(String koreanKeyword , String englishKeyword);
 }
