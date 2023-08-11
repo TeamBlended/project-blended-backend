3d98d08869813028943c7b6f6c8535aad3628e2e
jeonsangmin <jsangmin99@gmail.com>
2023-08-11 15:17:54 +0900

    TODO 제거

--- src/main/java/com/gdsc/blended/alcohol/repository/AlcoholRepository.java
+++ src/main/java/com/gdsc/blended/alcohol/repository/AlcoholRepository.java
@@ -8,6 +8,5 @@ import java.util.Optional;
 
 
 public interface AlcoholRepository extends JpaRepository<AlcoholEntity, Long> {
-    // TODO: 2023/08/10 영어 같은경우는 대소문자 구별을 못하는 문제 있음 알고리즘 수정해야함 ㅠㅠ
     List<AlcoholEntity> findByWhiskyKoreanContainingOrWhiskyEnglishContainingIgnoreCase(String koreanKeyword, String englishKeyword);
 }
