48327bbea79bcc6cea461324b2b7ef807e88f645
jeonsangmin <jsangmin99@gmail.com>
2023-08-20 00:21:09 +0900

    Response 수정

--- src/main/java/com/gdsc/blended/post/dto/PostResponseDto.java
+++ src/main/java/com/gdsc/blended/post/dto/PostResponseDto.java
@@ -58,6 +58,29 @@ public class PostResponseDto {
         this.author.setProfileImageUrl(postEntity.getUserId().getProfileImageUrl());
         this.image = image;
     }
+    public PostResponseDto(PostEntity postEntity, String image, PostInAlcoholEntity postInAlcoholEntity) {
+        this.id = postEntity.getId();
+        this.title = postEntity.getTitle();
+        this.content = postEntity.getContent();
+        this.shareLocation = new LocationDto();
+        this.shareLocation.setName(postEntity.getLocationName());
+        this.shareLocation.setLat(postEntity.getLatitude());
+        this.shareLocation.setLng(postEntity.getLongitude());
+        this.completed = postEntity.getCompleted();
+        this.createdAt = postEntity.getCreatedDate();
+        this.updatedAt = postEntity.getModifiedDate();
+        this.viewCount = postEntity.getViewCount();
+        this.scrapCount = postEntity.getLikeCount();
+        this.maxParticipantsCount = postEntity.getMaxRecruits();
+        this.shareDateTime = postEntity.getShareDateTime();
+        this.category = postEntity.getCategory().getId();
+        this.author = new AuthorDto();
+        this.author.setNickname(postEntity.getUserId().getNickname());
+        this.author.setProfileImageUrl(postEntity.getUserId().getProfileImageUrl());
+        this.image = image;
+        this.alcoholId = postInAlcoholEntity.getAlcoholEntity().getId();
+    }
+
 
     public PostResponseDto(PostEntity updatedPost) {
         this.id = updatedPost.getId();
