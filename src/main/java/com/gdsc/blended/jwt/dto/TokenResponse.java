2ed25d2e9b5bc73929369cff952357b5f3e34f40
jeonsangmin <jsangmin99@gmail.com>
2023-09-08 12:58:47 +0900

    에러를 반환하지않고 response에 키워드 추가

--- src/main/java/com/gdsc/blended/jwt/dto/TokenResponse.java
+++ src/main/java/com/gdsc/blended/jwt/dto/TokenResponse.java
@@ -1,8 +1,13 @@
 package com.gdsc.blended.jwt.dto;
 
+import org.springframework.http.HttpStatus;
+
 public record TokenResponse(
         String grantType,
         String accessToken,
         String refreshToken,
-        Long accessTokenExpiresIn
-) { }
+        Long accessTokenExpiresIn,
+        String message
+) {
+
+}
